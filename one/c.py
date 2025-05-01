c2fda9a1dc6e33e6169710b9542ce83354491793
poojasuthar456 <sutharpooja9913@gmail.com>
2025-05-01 16:35:25 +0530

    Python_First

---
+++ one/c.py
@@ -0,0 +1,17 @@
+print("We welcome you to our interactive perosnal data collector program!")
+
+name=input("Please enter your name:")
+age= int(input("Enter your age:"))
+height= float(input("Enter your height in meters:"))
+fav_number= int(input("Enter your favourite number:"))
+
+print("Thank you! here is the information we collected:")
+
+print("Name:", name , "(Type:", type(name),"Memory Address:" ,id(name),")")
+print("Age:", age , "(Type:", type(age),"Memory Address:" ,id(age),")")
+print("Name:", height , "(Type:", type(height),"Memory Address:" ,id(height),")")
+print("Name:", fav_number , "(Type:", type(fav_number),"Memory Address:" ,id(fav_number),")")
+
+print("your birthyear is approximately:" , 2025-age , "(based on your age of:", age,")")
+
+print("Thank you for using us, Goodbye!")
